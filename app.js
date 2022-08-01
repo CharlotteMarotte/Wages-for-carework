1df5790bba25bd784d58faf356a007141c2ca351
Charlotte Bilsing <Charlotte.Bilsing@posteo.de>
2022-08-01 15:34:58 +0200

    fixing errors

--- app.js
+++ app.js
@@ -23,10 +23,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/client/build'))); 
 
 // Location of static assets 
-app.use(express.static(path.join(__dirname, '/client/build'))); 
 
 app.use('/bill-cats', CategoriesRouter);
 app.use('/invoices', InvoiceRouter);
